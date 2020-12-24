b1695ff41d691cc70b5e07fe5926c91c844e6b94
Yang <twentynet@msn.com>
2020-12-25 00:18:05 +0900

    1
    처음 올려봅니다.

---
+++ src/main/java/com/ta/web/SampleController.java
@@ -0,0 +1,20 @@
+package com.ta.web;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping(value="/")
+    @ResponseBody
+    public String sampleHome() {
+
+        return "Hello Spring Boot!";
+
+    }
+}
+
